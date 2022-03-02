c6c16ac52fca48edfbbe31236f3015676506b5e7
maciejas22 <87901496+maciejas22@users.noreply.github.com>
2022-03-02 19:05:22 +0100

    Update main.c

--- main.c
+++ main.c
@@ -22,23 +22,23 @@ bool isInt(char *string) {
 
 void inputErrorHandling(int argc, char *argv[]){
     if(argc > 1 && argv[1] != "-r") {
-        printf("Nieprawidlowa flaga");
+        fprintf(stderr, "Nieprawidlowa flaga");
         exit(-1);
     }
     if(argc > 2 && isInt(argv[2]) == 0) { 
-        printf("Bledna ilosc wierszy");
+        fprintf(stderr, "Bledna ilosc wierszy");
         exit(-1);
     }
     if(argc > 3 && isInt(argv[3]) == 0) { 
-        printf("Bledna ilosc kolumn");
+        fprintf(stderr, "Bledna ilosc kolumn");
         exit(-1);
     }
     if(argc > 4 && isDouble(argv[4]) == 0) { 
-        printf("Bledny zakres wag krawedzi grafu");
+        fprintf(stderr, "Bledny zakres wag krawedzi grafu");
         exit(-1);
     }
     if(argc > 5 && isDouble(argv[5]) == 0) { 
-        printf("Bledny zakres wag krawedzi grafu");
+        fprintf(stderr, "Bledny zakres wag krawedzi grafu");
         exit(-1);
     }
     
@@ -52,12 +52,14 @@ int main(int argc, char *argv[]) {
     double min_weight_range = argc > 4 ? atof(argv[4]) : 0;
     double max_weight_range = argc > 5 ? atof(argv[5]) : 1; 
     
-    struct graph *grid = initGraph(4);
-    addEdge(grid, 0, 3 ,1.33213212);
-    addEdge(grid, 1, 3 ,1.32321);
-    addEdge(grid, 2, 3 ,1.32132);
-    addEdge(grid, 2, 3, 1.3);
-    writeGraphToFile(grid, file_name, 7, 3);
-    freeGraph(grid);
+    //readGraphFromFile(file_name);
+    // struct graph *grid = initGraph(rows*columns);
+    // addEdge(grid, 0, 3 ,1.33213212);
+    // addEdge(grid, 1, 3 ,1.32321);
+    // addEdge(grid, 2, 3 ,1.32132);
+    // addEdge(grid, 2, 3, 1.3);
+    // writeGraphToFile(grid, file_name, 7, 3);
+    // freeGraph(grid);
+    readGraphFromFile(file_name);
     return 0;
 }
