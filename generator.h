1210b5ab0e4992e6501a2a22bb8259dfed20b471
maciejas22 <motorosas2012@gmail.com>
2022-03-24 10:56:05 +0100

    wczesniej dodalem zly plik

--- generator.h
+++ generator.h
@@ -1,15 +1,10 @@
 #ifndef __GENERATOR_H__
 #define __GENERATOR_H__
 
-<<<<<<< HEAD
 #include <stdlib.h>
 #include <stdbool.h>
 
 double rnd(double min, double max);
 struct graph *generateGraph(struct graph *grid, int rows, int columns, double min_weight, double max_weight);
-=======
-double rnd(double min, double max);
-generateGraph(struct graph *grid, int rows, int columns, double min_weight, double max_weight);
->>>>>>> a2aead74406683988e1eeac352b8f6719638f00e
 
 #endif
