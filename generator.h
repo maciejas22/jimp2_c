dcd86fd74eec7e42e85fc71af077dfe30fdcf1e7
Tomasz Kowalski <t.kowalski999@gmail.com>
2022-03-24 08:58:27 +0100

    zmina nazwy funkcji

--- generator.h
+++ generator.h
@@ -1,7 +1,7 @@
 #ifndef __GENERATOR_H__
 #define __GENERATOR_H__
 
-double random(double min, double max);
+double rnd(double min, double max);
 generateGraph(struct graph *grid, int rows, int columns, double min_weight, double max_weight);
 
 #endif
