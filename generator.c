47bcb7d39a805eb39cdd50272d97a8f1d8c2b478
maciejas22 <motorosas2012@gmail.com>
2022-03-31 20:28:33 +0200

    obsluga grafow o 1 kolumnie i wierszu

--- generator.c
+++ generator.c
@@ -14,19 +14,23 @@ struct graph *generateGraph(struct graph *grid, int rows, int columns, double mi
     srand(time(NULL));
 
     // generowanie krawedzi poziomych
-    for(int level = 1; level <= rows; level++) {
+    if(columns > 1){
+        for(int level = 1; level <= rows; level++) {
         for(int i = level*columns-columns; i < level*columns-1; i++) {
             addEdge(grid, i, i+1, rnd(min_weight, max_weight));
             addEdge(grid, i+1, i, rnd(min_weight, max_weight));
         }
     }
+    }
 
     // generowanie krawedzi pionowych
+    if(rows > 1) {
     for(int level = 0; level < columns; level++) {
         for(int i = 0; i < rows-1; i++) {
             addEdge(grid, level + columns*i, level +columns*(i+1), rnd(min_weight, max_weight));
             addEdge(grid, level +columns*(i+1), level + columns*i, rnd(min_weight, max_weight));
         }
+    }
     }
 
     return grid;
